7ce58c32f67ad903c14c1e198d600ea09058ddc8
Bonifasius Ariesto Adrian Finantyo <adrian.finantyo@nusantara.technology>
2023-07-29 16:19:08 +0700

    fix: missing tag on solving conflict

--- resources/js/Pages/Game/Game.tsx
+++ resources/js/Pages/Game/Game.tsx
@@ -16,8 +16,6 @@ import WardrobeData from "@/Pages/Game/Wardrobe/WardrobeData.json";
 import AppLayout from "@/Layouts/AppLayout";
 
 export default function Game({ ItemData }: any) {
-
-
     var Sound = new Howl({
         src: [ClickSfx],
     });
@@ -28,25 +26,44 @@ export default function Game({ ItemData }: any) {
 
     // placeholder
     const wardrobe = WardrobeData[0];
-    // 
+    //
     useEffect(() => {
         console.log(ItemData);
-    }, [])
-    return <div
-        id="Game_page">
-        <img src={BG} id="bg" />
-        <Chest />
-        <Avatar outfit={wardrobe} />
-        <div id="Navigate">
-            <Link onClick={() => { Sound.play() }} href="/game/minigames" className="ButtonNav">
-                <img src="https://www.svgrepo.com/show/95376/game-controller.svg" alt="" />
-                {/* Mini Games */}
-            </Link>
-            <Link onClick={() => { soundTwo.play() }} href="/game/wardrobe" className="ButtonNav">
-                <img src="https://www.svgrepo.com/show/234613/clothes-shirt.svg" alt="" />
-                {/* Wardrobe */}
-            </Link>
+    }, []);
 
+    return (
+        <div id="Game_page">
+            <img src={BG} id="bg" />
+            <Chest />
+            <Avatar outfit={wardrobe} />
+            <div id="Navigate">
+                <Link
+                    onClick={() => {
+                        Sound.play();
+                    }}
+                    href="/game/minigames"
+                    className="ButtonNav"
+                >
+                    <img
+                        src="https://www.svgrepo.com/show/95376/game-controller.svg"
+                        alt=""
+                    />
+                    {/* Mini Games */}
+                </Link>
+                <Link
+                    onClick={() => {
+                        soundTwo.play();
+                    }}
+                    href="/game/wardrobe"
+                    className="ButtonNav"
+                >
+                    <img
+                        src="https://www.svgrepo.com/show/234613/clothes-shirt.svg"
+                        alt=""
+                    />
+                    {/* Wardrobe */}
+                </Link>
+            </div>
         </div>
     );
 }
